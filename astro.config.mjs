e95c6e0062526f3206095e136f645b8f3e81f2e1
Chris Fretz <chrisfretz@gmail.com>
2026-09-09 09:04:51 -0400

    Add fonts and sass files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [
+        {
+            provider: fontProviders.fontsource(),
+            name: "IBM Plex Mono",
+            cssVariable: "--font-ibm-plex-mono",
+            weights: ["300 400 500 600 700"],
+            fallbacks: ["monospace"]
+        },
+        {
+            provider: fontProviders.fontsource(),
+            name: "Pacifico",
+            cssVariable: "--font-pacifico",
+            fallbacks: ["cursive"]
+        }
+    ]
+});
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,4 +1,5 @@
 ---
 title: 'Homepage'
+layout: '../layouts/BaseLayout.astro'
 ---
 Here's some filler content to get us started.
