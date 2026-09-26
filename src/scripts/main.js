8a3740f40a8d432c9178f04369ab86cdbd8be22c
Chris Fretz <chrisfretz@gmail.com>
2026-09-26 10:40:57 -0400

    Update header and footer, create social icons component

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 "use strict";
 
+// Define variables
+const mobileMenuButton = document.getElementById('menu-toggle');
+
 // Mobile Menu event handler
+const handleMobileMenuClick = function() {
+    if (mobileMenuButton) {
+        mobileMenuButton.addEventListener('click', (e) => {
+            e.stopPropagation();
+            document.body.querySelector('.nav').classList.toggle('open');
+        })
+    }
+}
+
+// Function to close menu when clicking outside
+const handleClickOutside = function() {
+    document.addEventListener('click', (e) => {
+        const nav = document.querySelector('.nav');
+
+        if (nav && nav.classList.contains('open')) {
+            // Check if the click was outside the nav element
+            if (!nav.contains(e.target)) {
+                nav.classList.remove('open');
+            }
+        }
+    })
+}
+
+// Displays current year
+const displayCurrentYear = () => {
+    const year = document.getElementById('year');
+    if (year) {
+        year.textContent = new Date().getFullYear();
+    }
+}
+
+// Wait for the DOM to be fully loaded before running functions
+document.addEventListener('DOMContentLoaded', function() {
+    handleMobileMenuClick();
+    handleClickOutside();
+    displayCurrentYear();
+});
